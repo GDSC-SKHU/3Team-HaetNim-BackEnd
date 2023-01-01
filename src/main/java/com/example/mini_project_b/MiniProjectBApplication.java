940d9f871023e19dfaabfd297e36bdb86f6f47d5
LEEHYUNBOK <gusqhrzkq@naver.com>
2023-01-01 22:33:12 +0900

    Feat : 3팀 백엔드 기초 생성

---
+++ src/main/java/com/example/mini_project_b/MiniProjectBApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_project_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectBApplication.class, args);
+	}
+
+}
