ed38362c225f81ad60fff87dd139a5f0c26c2ed0
tndusy27 <tndusy527@gmail.com>
2023-01-04 14:45:11 +0900

    dto

--- src/main/java/com/example/mini_project_b/login/domain/Member.java
+++ src/main/java/com/example/mini_project_b/login/domain/Member.java
@@ -1,6 +1,7 @@
 package com.example.mini_project_b.login.domain;
 
 
+import com.example.mini_project_b.follow.DTO.Follow;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,7 +9,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -21,13 +21,34 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 @Entity
 public class Member implements UserDetails {
-
+    @GeneratedValue
     @Id
-    @Column(updatable = false, unique = true, nullable = false)
-    private String memberId;
-
-    @Column(nullable = false)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "password", nullable = false, length = 100)
     private String password;
+
+    @Column(name="profileImg", nullable = true,length = 200)
+    private String profileImg;
+
+    @Column(name="nickname",nullable = false,length = 100)
+    private String nickname;
+
+    @Column(name="statusMessage",nullable = false,length = 100)
+    private String statusMessage;
+
+    //    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+//    @JoinTable(name="follow")
+//    private List<Follow> follows;
+    @OneToMany(mappedBy = "follower",fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Follow> followerList;
+
+    @OneToMany(mappedBy = "followee",fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private  List<Follow> followeeList;
+
+    @OneToMany(fetch=FetchType.LAZY, cascade={CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name="post_likes", joinColumns=@JoinColumn(name="user_id"), inverseJoinColumns=@JoinColumn(name="post_id"))
+    private List<Member> members;
 
     @ElementCollection(fetch = FetchType.EAGER)
     @Builder.Default
@@ -39,34 +60,34 @@ public class Member implements UserDetails {
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
     }
-
     @Override
     public String getUsername() {
-        return memberId;
+        return getUsername();
     }
-
     @Override
     public String getPassword() {
         return password;
     }
-
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
-
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
-
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
-
     @Override
     public boolean isEnabled() {
         return true;
     }
 }
+/**
+ * - user : post = 1 : n
+ * - user : user_post_like = 1: n
+ * - user : follow = n : m
+ * - post : post_like = 1 : n
+ */
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,8 @@
 use jwt;
 
-insert into member(member_id, password) values ('member_A', '1234');
-insert into member_roles(member_member_id, roles) values ('member_A', 'USER');
+# insert into member(member_id, password) values ('member_A', '1234');
+# insert into member_roles(member_member_id, roles) values ('member_A', 'USER');
+
+insert into post_likes(user_id,post_id)values (0,0);
+insert into follow(follower_Id,following_Id) values (9,2);
+
