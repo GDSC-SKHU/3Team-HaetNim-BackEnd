808f82edf0db0c782b085ee511634534c82c2232
LEEHYUNBOK <gusqhrzkq@naver.com>
2023-01-11 20:03:04 +0900

    Fix : Cors 정책 오류 수정

--- src/main/java/com/example/mini_project_b/MiniProjectBApplication.java
+++ src/main/java/com/example/mini_project_b/MiniProjectBApplication.java
@@ -2,7 +2,10 @@ package com.example.mini_project_b;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -11,5 +14,14 @@ public class MiniProjectBApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MiniProjectBApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")// 이 경로에 접근하는
+						.allowedOrigins("*"); // 해당 Origin에 대해 CORS 활성화
+			}
+		};
+	}
 }
